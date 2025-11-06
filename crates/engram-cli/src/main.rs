b50d0679ac37b5a5d45a47e0a84976381d209fbf
jason13official <jason13official@gmail.com>
2025-11-06 14:19:22 -0500

    feat: read contents of file line-by-line

--- crates/engram-cli/src/main.rs
+++ crates/engram-cli/src/main.rs
@@ -1,17 +1,25 @@
-use std::path::PathBuf;
 use clap::Parser;
+use std::fs::read_to_string;
+use std::path::PathBuf;
 
 #[derive(Parser)]
 struct CliArguments {
     pattern: String,
-    path: PathBuf
+    path: PathBuf,
 }
 
 fn main() {
-
     let args = CliArguments::parse();
 
-    println!("PASSED ARGUMENTS TO ENGRAM CLI: ");
-    println!("-> pattern: {:?}", args.pattern);
-    println!("-> path: {:?}", args.path);
+    // println!("PASSED ARGUMENTS TO ENGRAM CLI: ");
+    // println!("-> pattern: {:?}", args.pattern);
+    // println!("-> path: {:?}", args.path);
+
+    let content = read_to_string(&args.path).expect("could not read file ");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 }
