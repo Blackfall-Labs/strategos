f9e5a6c9fe6cdbedad28fc08e2f9af82da47aab7
jason13official <jason13official@gmail.com>
2025-11-06 14:28:59 -0500

    feat: use "?" for conciseness

--- crates/engram-cli/src/main.rs
+++ crates/engram-cli/src/main.rs
@@ -8,18 +8,50 @@ struct CliArguments {
     path: PathBuf,
 }
 
-fn main() {
+// fn main() {
+//     let args = CliArguments::parse();
+//
+//     // println!("PASSED ARGUMENTS TO ENGRAM CLI: ");
+//     // println!("-> pattern: {:?}", args.pattern);
+//     // println!("-> path: {:?}", args.path);
+//
+//     let result = std::fs::read_to_string("test.txt");
+//     let content = match result {
+//         Ok(content) => { content },
+//         Err(error) => { panic!("Can't deal with {}, just exit here", error); }
+//     };
+//     println!("file content: {}", content);
+//     let content = std::fs::read_to_string("test.txt").unwrap();
+//
+//     for line in content.lines() {
+//         if line.contains(&args.pattern) {
+//             println!("{}", line);
+//         }
+//     }
+// }
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
     let args = CliArguments::parse();
 
-    // println!("PASSED ARGUMENTS TO ENGRAM CLI: ");
-    // println!("-> pattern: {:?}", args.pattern);
-    // println!("-> path: {:?}", args.path);
+    // verbose, not pretty
+    // let result = read_to_string(&args.path);
+    // let content = match result {
+    //     Ok(content) => { content },
+    //     Err(error) => { return Err(error.into()); }
+    // };
 
-    let content = read_to_string(&args.path).expect("could not read file ");
+    // concise, the "?" operator handles error catching
+    let content = read_to_string(&args.path)?;
+
+    // prints the entire raw contents of the file
+    // println!("file content: {}", content);
 
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+
+    Ok(())
 }
