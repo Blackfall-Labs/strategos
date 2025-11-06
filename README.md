db9251edfcf6fffad254c02ae6b7f6ee455fbe02
jason13official <jason13official@gmail.com>
2025-11-06 14:00:08 -0500

    feat: verbose log, update readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # engram-cli
+
+---
+
+### Testing
+
+> `cargo run -- MIT LICENSE`
+
+```text
+PASSED ARGUMENTS TO ENGRAM CLI:
+-> binary: "target\\debug\\engram-cli.exe"
+-> pattern: "MIT"
+-> path: "LICENSE"
+```
--- crates/engram-cli/src/main.rs
+++ crates/engram-cli/src/main.rs
@@ -2,8 +2,12 @@ use engram_core::ArchiveReader;
 
 fn main() {
 
+    let binary = std::env::args().nth(0).expect("no binary given");
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path given");
 
-    println!("pattern: {:?}, path: {:?}", pattern, path)
+    println!("PASSED ARGUMENTS TO ENGRAM CLI: ");
+    println!("-> binary: {:?}", binary);
+    println!("-> pattern: {:?}", pattern);
+    println!("-> path: {:?}", path);
 }
